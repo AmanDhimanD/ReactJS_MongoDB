2c9180badfb3461fd13e2e7b61d26e2866bcaf82
Dhiman_aman <72404186+AmanDhimanD@users.noreply.github.com>
2023-02-25 11:19:10 +0530

    Add files via upload

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("./db/config");
+const User = require("./db/User");
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.post("/register", async (req, res) => {
+  let user = new User(req.body);
+  let result = await user.save();
+  result = result.toObject();
+  delete result.password;
+  res.send(result);
+});
+app.post("/login",async (req, res) => {
+ let user = new User(req.body)
+    let result = await user.save()
+    res.send(result)
+})
+
+
+app.listen(5000);
